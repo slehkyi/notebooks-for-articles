541b3eb87739131391f63dcd1e90c17ad003b525
Sergi <s.lehkyi@cronostelecom.com>
2018-11-04 20:04:55 +0100

    plots

--- time-checks.py
+++ time-checks.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
 
 np.random.seed(666)
 a_min = np.random.binomial(28, 0.044, size=10000)
@@ -40,3 +44,29 @@ for _ in range(sample):
                 n_sequential += 1
 
 print("Probability of two rare events one after another: " + str(float(n_sequential/sample)))
+
+bins_min = np.arange(0, max(a_min) + 1.5) - 0.5
+bins_avg = np.arange(0, max(a_avg) + 1.5) - 0.5
+bins_max = np.arange(0, max(a_max) + 1.5) - 0.5
+
+# plt.subplot(3, 1, 1)
+plt.hist(a_min, bins=bins_min, normed=True, color='red')
+plt.title('Minimum phone usage')
+plt.xlabel('Amount of "lucky hours spotted during the day"')
+plt.ylabel('Probability')
+plt.show()
+
+# plt.subplot(3, 1, 2)
+plt.hist(a_avg, bins=bins_avg, normed=True, color='green')
+plt.title('Average phone usage')
+plt.xlabel('Amount of "lucky hours spotted during the day"')
+plt.ylabel('Probability')
+plt.show()
+
+# plt.subplot(3, 1, 3)
+plt.hist(a_max, bins=bins_max, normed=True, color='blue')
+plt.title('Maximum phone usage')
+plt.xlabel('Amount of "lucky hours spotted during the day"')
+plt.ylabel('Probability')
+plt.show()
+
