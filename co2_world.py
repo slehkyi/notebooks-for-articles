a0452ee3a6cddafc9cdefcf74fb80b202def8b3f
Sergi <s.lehkyi@cronostelecom.com>
2019-01-27 14:37:27 +0100

    Update co2_world.py

--- co2_world.py
+++ co2_world.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from bokeh.io import output_file, show, curdoc
 from bokeh.plotting import figure
@@ -17,7 +18,8 @@ gapminder = pd.read_csv('data/gapminder_tidy.csv')
 gapminder.head()
 
 df = gapminder[['Country', 'region']].drop_duplicates()
-data_with_regions = pd.merge(data, df, left_on='country', right_on='country', how='inner')
+data_with_regions = pd.merge(data, df, left_on='country', right_on='Country', how='inner')
+data_with_regions = data_with_regions.drop('Country', axis='columns')
 
 data_with_regions.head()
 
@@ -41,6 +43,10 @@ final_df = pd.merge(upd_new_df, df_gdp, on=['country', 'year'], how='left')
 final_df = final_df.dropna()
 final_df.head()
 
+np_co2 = np.array(final_df['co2'])
+np_gdp = np.array(final_df['gdp'])
+np.corrcoef(np_co2, np_gdp)
+
 # Creating visualization app with Bokeh.io
 regions_list = final_df.region.unique().tolist()
 color_mapper = CategoricalColorMapper(factors=regions_list, palette=Spectral6)
@@ -60,18 +66,21 @@ xmin, xmax = min(final_df.gdp), max(final_df.gdp)
 ymin, ymax = min(final_df.co2), max(final_df.co2)
 
 # Create the figure: plot
-plot = figure(title='Gapminder Data for 1964', plot_height=400, plot_width=700,
-              x_range=(xmin, xmax), y_range=(ymin, ymax))
+plot = figure(title='Gapminder Data for 1964', plot_height=600, plot_width=1000,
+              x_range=(xmin, xmax),
+              y_range=(ymin, ymax), y_axis_type='log',)
 
 # Add circle glyphs to the plot
 plot.circle(x='x', y='y', fill_alpha=0.8, source=source, legend='region',
-            color=dict(field='region', transform=color_mapper))
+            color=dict(field='region', transform=color_mapper),
+            size=7)
 
 # Set the legend.location attribute of the plot to 'top_right'
-plot.legend.location = 'top_right'
+plot.legend.location = 'bottom_right'
 
 # Set the x-axis label
-plot.xaxis.axis_label = 'GDP'
+plot.xaxis.axis_label = 'Income per person (Gross domestic product per person adjusted for differences in ' \
+                        'purchasing power in international dollars, fixed 2011 prices, PPP based on 2011 ICP)'
 
 # Set the y-axis label
 plot.yaxis.axis_label = 'CO2 emissions (tonnes per person)'
@@ -80,11 +89,6 @@ plot.yaxis.axis_label = 'CO2 emissions (tonnes per person)'
 def update_plot(attr, old, new):
     # set the `yr` name to `slider.value` and `source.data = new_data`
     yr = slider.value
-    #x = x_select.value
-    #y = y_select.value
-    # Label axes of plot
-    #plot.xaxis.axis_label = x
-    #plot.yaxis.axis_label = y
 
     new_data = {
         'x': final_df.gdp[final_df['year'] == yr],
@@ -93,12 +97,6 @@ def update_plot(attr, old, new):
         'region': final_df.region[final_df['year'] == yr],
     }
     source.data = new_data
-
-    # Set the range of all axes
-    #plot.x_range.start = min(data[x])
-    #plot.x_range.end = max(data[x])
-    #plot.y_range.start = min(data[y])
-    #plot.y_range.end = max(data[y])
 
     # Add title to figure: plot.title.text
     plot.title.text = 'Gapminder data for %d' % yr
@@ -111,7 +109,7 @@ slider = Slider(start=1964, end=2013, step=1, value=1964, title='Year')
 slider.on_change('value', update_plot)
 
 # Create a HoverTool: hover
-hover = HoverTool(tooltips=[('Country', '@country')])
+hover = HoverTool(tooltips=[('Country', '@country'), ('GDP', '@x'), ('CO2 emission', '@y')])
 
 # Add the HoverTool to the plot
 plot.add_tools(hover)
